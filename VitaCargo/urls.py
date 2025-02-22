78d8360334da188d35ed95632d64bd6e745becb3
1calamity3 <saparbekov.nova@gmail.com>
2025-02-22 11:55:17 -0600

    Update urls.py

--- VitaCargo/urls.py
+++ VitaCargo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import set_language
+from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
@@ -10,3 +11,6 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('', include('profile.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += i18n_patterns(
+    path('set_language/', 'django.views.i18n.set_language', name='set_language'),
+)
